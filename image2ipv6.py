550076926b253a29067fb10e0dbeffc2ce671664
Kyle <kpz@ens160.com>
2018-12-19 21:38:58 -0800

    Create image2ipv6.py

---
+++ image2ipv6.py
@@ -0,0 +1,14 @@
+import sys
+from PIL import Image
+
+img = Image.open(sys.argv[1])
+img = img.convert('RGB')
+
+for y in range(img.height):
+    for x in range(img.width):
+        r, g, b = img.getpixel((x, y))
+        r_hex='%02x' % r
+        g_hex='%02x' % g
+        b_hex='%02x' % b
+        address = '2001:4c08:2028:'+str(x+100)+':'+str(y+68)+':'+r_hex+':'+g_hex+':'+b_hex;
+        print(address)
